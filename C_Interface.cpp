8a29a2be8373d75349c379c222699ab7c087107d
1KitCat1 <denischool119@gmail.com>
2021-10-26 18:08:24 +0300

    Add const and link transfer

--- C_Interface.cpp
+++ C_Interface.cpp
@@ -2,26 +2,31 @@
 
 constexpr int64_t MODULUS = 1000000007;
 
-struct inputData{
-    int64_t n, m, k;
+struct InputData {
+    int64_t amountOfSteps;
     std::vector<std::vector<int64_t>> matrix;
 };
 
-std::vector<std::vector<int64_t>> multiplyMatrix(
-    const std::vector<std::vector<int64_t>> matrixLeft, 
-    const std::vector<std::vector<int64_t>> matrixRight);
+std::vector<std::vector<int64_t>> multiplyMatrices(
+    const std::vector<std::vector<int64_t>>& matrixLeft, 
+    const std::vector<std::vector<int64_t>>& matrixRight);
 
 std::vector<std::vector<int64_t>> powerMatrix(
-    const std::vector<std::vector<int64_t>> matrix, 
+    const std::vector<std::vector<int64_t>>& matrix, 
     const int64_t exponent);
 
-int64_t getNumberOfWays(std::vector<std::vector<int64_t>> matrix, int64_t k);
+int64_t getNumberOfWays(
+	const std::vector<std::vector<int64_t>>& matrix,
+	const int64_t k);
 
-inputData inputRead(std::istream& in);
+InputData inputRead(std::istream& in);
+
+void writeAnswer(std::ostream& out, int64_t result);
 
 signed main() {
-    inputData input = inputRead(std::cin);
-    std::cout << getNumberOfWays(input.matrix, input.k);
+    InputData input = inputRead(std::cin);
+    int64_t result = getNumberOfWays(input.matrix, input.amountOfSteps);
+    writeAnswer(std::cout, result);
 
     return 0;
 }
