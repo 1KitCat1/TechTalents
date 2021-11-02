b3b900316b6510ff3c2596afb8ff291d9c732764
1KitCat1 <denischool119@gmail.com>
2021-11-02 22:53:09 +0200

    Add CodeReview

---
+++ C_CodeReview.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+constexpr int64_t MODULUS = 1000000007;
+
+struct InputData {
+    int64_t amountOfSteps;
+    std::vector<std::vector<int64_t>> matrix;
+};
+
+InputData inputRead(std::istream& in){
+    InputData  input;
+    int n, m, k;
+    std::cin >> n >> m >> k;
+    input.amountOfSteps = k;
+    input.matrix = std::vector<std::vector<int64_t>>
+                    (n, std::vector<int64_t>(n));
+
+    for (int i = 0; i < m; i++) {
+        int from;
+        int to;
+        in >> from >> to;
+        input.matrix[from][to]++;
+    }
+    return input;
+}
+
+void writeAnswer(std::ostream& out, int64_t result){
+    out << result << std::endl;
+}
+
+std::vector<std::vector<int64_t>> multiplyMatrices(
+    const std::vector<std::vector<int64_t>>& matrixLeft,
+    const std::vector<std::vector<int64_t>>& matrixRight){
+
+    if (matrixLeft.size() == 0 || matrixRight.size() == 0) {
+        throw std::invalid_argument("Matrix is empty");
+    }
+
+    for (int index = 1; index < matrixLeft.size(); index++) {
+        if (matrixLeft[index].size() != matrixLeft[0].size()) {
+            throw std::invalid_argument(
+                "Vector which represent matrix is NOT rectangular");
+        }
+    }
+    for (int index = 1; index < matrixRight.size(); index++) {
+        if (matrixLeft[index].size() != matrixRight[0].size()) {
+            throw std::invalid_argument(
+                "Vector which represent matrix is NOT rectangular");
+        }
+    }
+
+    if (matrixLeft[0].size() != matrixRight.size()) {
+        throw std::invalid_argument(
+            "Given matrices cannot be multiplied due to their size");
+    }
+
+    std::vector<std::vector<int64_t>> answ(matrixLeft.size(), 
+        std::vector<int64_t>(matrixRight[0].size(), 0));
+
+
+
+    for (int i = 0; i < matrixLeft.size(); i++) {
+        for (int j = 0; j < matrixRight[0].size(); j++) {
+            for (int k = 0; k < matrixLeft[0].size(); k++) {
+                answ[i][j] += (matrixLeft[i][k]*matrixRight[k][j]) % MODULUS;
+                answ[i][j] %= MODULUS;
+            }
+        }
+    }
+    return answ;
+}
+
+std::vector<std::vector<int64_t>> powerMatrix(
+    const std::vector<std::vector<int64_t>>& matrix,
+    const int64_t exponent){
+
+    if (matrix.size() == 0) {
+        throw std::invalid_argument("Matrix is empty");
+    }
+
+    for (int index = 1; index < matrix.size(); index++) {
+        if (matrix[index].size() != matrix[0].size()) {
+            throw std::invalid_argument(
+                "Vector which represent matrix is NOT rectangular");
+        }
+    }
+
+
+    if (exponent == 0) {
+        int n = matrix.size();
+        std::vector<std::vector<int64_t>> matrixE(
+            matrix.size(), std::vector<int64_t>(matrix.size(), 0));
+
+        for (int i = 0; i < matrix.size(); i++) {
+            matrixE[i][i] = 1;
+        }
+
+        return matrixE;
+    }
+
+    std::vector<std::vector<int64_t>> answerMatrix;
+    if (exponent % 2 == 0) {
+        answerMatrix = powerMatrix(matrix, exponent/2);
+        return multiplyMatrices(answerMatrix, answerMatrix);
+    }else {
+        answerMatrix = multiplyMatrices(powerMatrix(matrix, exponent-1), matrix);
+    }
+    return answerMatrix;
+}
+
+int64_t getNumberOfWays(
+    const std::vector<std::vector<int64_t>>& matrix,
+    const int64_t amountOfSteps){
+
+    int n = matrix.size();
+    std::vector<std::vector<int64_t>> answMatrix =
+            powerMatrix(matrix, amountOfSteps);
+
+    int answer = 0;
+    for (int index = 0; index < matrix.size(); index++) {
+        answer += answMatrix[0][index];
+        answer %= MODULUS;
+    }
+    return answer;
+}
+
+
+signed main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    
+    const InputData input = inputRead(std::cin);
+
+    const int64_t result = 
+            getNumberOfWays(input.matrix, input.amountOfSteps);
+    writeAnswer(std::cout, result);
+
+    return 0;
+}
